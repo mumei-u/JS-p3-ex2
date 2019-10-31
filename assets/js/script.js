3dd267d1ef8351c4db1db90fd608f3b584db464a
thyago_lbt <thyago14@outlook.fr>
2019-10-31 11:37:04 +0100

    fin

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// let visible = document.getElementById('visible');
+// let hide = document.getElementById('hide');
+// let text = document.getElementById('text');
+//
+// visible.onclick = function(){
+//   text.style.display = 'visible'
+// }
+//
+// hide.onclick = function(){
+//   text.style.display = 'hidden'
+// }
+
+function visible() {
+    document.getElementById('text').style.display = 'visibility';
+  }
+  function hide() {
+      document.getElementById('text').style.display = 'none';
+    }
